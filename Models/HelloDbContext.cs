fab61ec782ff12fbec0960ea10753ad7c455bbb3
海蜇零 <30751915+JellyfishZero@users.noreply.github.com>
2026-07-09 17:54:03 +0800

    feat 產生資料表model格式。

---
+++ Models/HelloDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_HelloWorld.Models;
+
+public partial class HelloDbContext : DbContext
+{
+    public HelloDbContext(DbContextOptions<HelloDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<News> News { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<News>(entity =>
+        {
+            entity.Property(e => e.NewsId).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.EndDateTime).HasColumnType("datetime");
+            entity.Property(e => e.InsertDateTime).HasColumnType("datetime");
+            entity.Property(e => e.StartDateTime).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(250);
+            entity.Property(e => e.UpdateDateTime).HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
